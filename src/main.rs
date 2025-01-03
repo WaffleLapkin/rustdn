34b7ee149d22a7ed78c0a0a9acf76a21bf0b600e
Waffle Lapkin <waffle.lapkin@gmail.com>
2025-01-03 12:47:00 +0100

    simplify matching on arg0 a lil bit

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,16 @@ fn main() {
         .as_deref()
         .map(Path::new)
         .and_then(|a| a.file_stem())
-        .and_then(OsStr::to_str);
+        .and_then(OsStr::to_str)
+        // Edge-case: no arg0 (or it's last part is not utf-8!)
+        .expect("No arg0 or arg0 is not utf8?");
 
     // `rustdn` is a "chimera binary" -- it changes behavior depending on the name of the
     // binary name (arg0). This is used to enable rustup-style "proxies" -- you can symlink `rustc`
     // to `rustdn` and `rustdn` will choose an appropriate `rustc` version and run it.
     match bin {
-        Some("rustdn") => rustdn::main(args),
-        Some(tool) => proxy::main(tool, args),
-
-        // Edge-case: no arg0 (or it's last part is not utf-8!)
-        None => panic!("No arg0?"),
+        "rustdn" => rustdn::main(args),
+        tool => proxy::main(tool, args),
     }
 }
 
